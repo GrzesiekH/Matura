b3b3c8a4d63ae2caae61abdbdcaa5ab86840813a
GrzesiekH <ghonowski@gmail.com>
2022-11-09 13:40:14 +0100

    zad jezyki bazy danych matura czerwiec 2020 zrobione zadania 1 do 4

--- 11_03_22/ad5.sql
+++ 11_03_22/ad5.sql
@@ -1,2 +1,4 @@
 -- select Rodzina, count(*) from jezyki group by Rodzina -- zad 1
--- select count(*) from  jezyki as j where jezyk not in (select jezyk from uzytkownicy where urzedowy = "tak" group by jezyk)
+-- select count(*) from  jezyki as j where jezyk not in (select jezyk from uzytkownicy where urzedowy = "tak" group by jezyk) -- zad 2
+-- select jezyk, count(*) as c from (select distinct kontynent, Jezyk from uzytkownicy as u join panstwa as p using(panstwo)) as t group by jezyk -- zad 3
+select jezyk, sum(Uzytkownicy) as suma from (select * from uzytkownicy join jezyki using(jezyk) where rodzina != "indoeuropejska") as t join panstwa using(panstwo) where Kontynent like "Ameryka%" group by jezyk order by suma desc limit 6
