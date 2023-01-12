c3763a4376bf0bc9dc6a861cf4a5b425d2b5818e
GrzesiekH <ghonowski@gmail.com>
2023-01-12 10:13:44 +0100

    zad 1 maj 2022 papier done i zad 2 maj 2022 done 1

--- trymestr_II/23_01_05/zad1_2.py
+++ trymestr_II/23_01_05/zad1_2.py
@@ -2,16 +2,13 @@ n = int(input())
 A = list(map(int,input().split()))
 L = []
 for i in range(n):
-    L.append(0)
+    L.append(i+1)
 counter  = 0
-for x in range(0,n):
-    if A[x] > n:
-        counter += 1
-    
-    elif L[A[x]] == 0:
-        L[A[x]] = 1
-for e in L:
-    if e == 0:
-        counter += 1
+for x in L:
+    for y in A:
+        if x == y:
+            counter -=1
+            break 
+    counter += 1
 
 print(counter)
